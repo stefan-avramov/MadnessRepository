4894e50c21d550aeb4bb07e7564856e2c2c78b88
Stefan Avramov <st3f.1p@gmail.com>
2012-11-14 01:09:45 +0200

    add missing file

---
+++ GooseUnrunnableRunsWPF/GooseUnrunnableRuns/Common/Images/ImagesContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Common
+{
+	public static class ImagesContainer
+	{
+		public static IList<ImageSource> GooseRunRightImages = new List<ImageSource>()
+			{
+				new BitmapImage(new Uri("pack://application:,,,/Common;component/Images/Goose_right_1.png")),
+				new BitmapImage(new Uri("pack://application:,,,/Common;component/Images/Goose_right_2.png"))
+			};
+
+		public static IList<ImageSource> GooseRunLeftImages = new List<ImageSource>()
+			{
+				new BitmapImage(new Uri("pack://application:,,,/Common;component/Images/Goose_left_1.png")),
+				new BitmapImage(new Uri("pack://application:,,,/Common;component/Images/Goose_left_2.png"))
+			};
+	}
+}
